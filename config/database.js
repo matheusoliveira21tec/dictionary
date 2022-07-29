50dc35328a5ee996a26e2255e981a29a19582c73
monegasco <mhoa.2000@gmail.com>
2022-07-29 00:25:17 -0300

    first commit

---
+++ config/database.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+main().then(() => console.log("Conection Sucessful."));
+
+async function main() {
+    await mongoose.connect('mongodb://localhost/javascriptNote');
+}
