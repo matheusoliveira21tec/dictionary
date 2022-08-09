70cafe865358cd4cc80682cfa8f710192fbf541b
monegasco <mhoa.2000@gmail.com>
2022-08-09 00:12:51 -0300

    first deploy

--- config/database.js
+++ config/database.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 main().catch(err => console.log(err));
 main().then(() => console.log("Conection Sucessful."));
-
+require('dotenv').config();
+const MONGO_URL = process.env.MONGO_URL;
 async function main() {
-    await mongoose.connect('mongodb://localhost/javascriptNote',
+    await mongoose.connect(MONGO_URL,
     );
 }
