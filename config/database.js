df7bd91a8504cfb3a2a4c5ac6313f6523fa0c820
monegasco <mhoa.2000@gmail.com>
2022-08-31 23:48:14 -0300

    deploy  commit

--- config/database.js
+++ config/database.js
@@ -1,9 +1,20 @@
-const mongoose = require('mongoose');
+//const mongoose = require('mongoose');
 
-main().catch(err => console.log(err));
-main().then(() => console.log("Conection Sucessful."));
-require('dotenv').config();
 
-async function main() {
-    mongoose.connect('mongodb://localhost/dictionary');
-}
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://admin:coodesh@cluster0.wprrwuv.mongodb.net";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+
+//main().catch(err => console.log(err));
+//main().then(() => console.log("Conection Sucessful."));
+//require('dotenv').config();
+
+//async function main() {
+ //   mongoose.connect('mongodb://localhost/dictionary');
+//}
